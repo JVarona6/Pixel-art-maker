9cd70460c4deef402d326405159e7c71ea9e664a
John Varona <johnvarona6@gmail.com>
2020-11-04 11:07:37 -0500

    Full grid and coloring functionality

--- designs.js
+++ designs.js
@@ -1,15 +1,12 @@
-// Select color input
-// Select size input
+var gridTable = document.getElementById("pixelCanvas");
+var submitButton = document.getElementById("btn");
 
-// When size is submitted by the user, call makeGrid()
-const gridTable = document.getElementById("pixelCanvas");
-const submitButton = document.getElementById("btn");
-
-
+// Function creates dynamic grid using input from user
 function makeGrid() {
     var inputHeight = document.getElementById("inputHeight").value;
     var inputWidth = document.getElementById("inputWidth").value;
-    while (gridTable.childElementCount > 0){
+
+    while (gridTable.firstChild){
         gridTable.removeChild(gridTable.firstChild)
     }
     for (let x = 1; x <= inputHeight; x++){
@@ -19,12 +16,19 @@ function makeGrid() {
         for (let z = 1; z <= inputWidth; z++){
             var tableColumn = document.createElement('td');
             tableRow.appendChild(tableColumn);
+
+
+//Fills in TD with chosen color????????
+            tableColumn.addEventListener('click', function(){
+                var chosenColor = document.getElementById("colorPicker");
+                this.style.backgroundColor = chosenColor.value;
+            })
         }
     }
 }
+
 
 submitButton.addEventListener('click', function(e){
     e.preventDefault()
     makeGrid()
 });
-
